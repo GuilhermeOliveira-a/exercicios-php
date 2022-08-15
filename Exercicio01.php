b732335acfcbfbdecb8885718d8fb80fb71c0d43
GuilhermeOliveira-a <nelson.domiciano.nd@gmail.com>
2022-08-14 22:39:06 -0300

    Partindo do basico de lógica de programação

---
+++ Exercicio01.php
@@ -0,0 +1,4 @@
+<?php
+// Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"
+echo "Olá Mundo!";
+?>
---
+++ Exercicio02.php
@@ -0,0 +1,4 @@
+<?php
+//Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boasvindas para ela
+
+$nome = 
